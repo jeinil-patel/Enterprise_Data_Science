f3ac4d36afeffb84d239cb6caef1f47df5b03aeb
Jeinil Patel <jeinilpatel44@gmail.com>
2020-08-22 14:08:17 +0200

    COVID-19 initial project commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Applied data science project for COVID-19 spread over different countries.',
+    author='Jeinil',
+    license='',
+)
